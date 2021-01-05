b7e907f6a2172f04a903864ce473f5cefdc777ea
otg08 <otgoo3456@gmail.com>
2021-01-04 23:13:37 -0800

    SHOOG SHIDDEG BOLGOH

--- app.js
+++ app.js
@@ -8,20 +8,29 @@ var scope = [0, 0];
 var roundScore = 0;
 
 //шоог аль талаараа буусныг хадгалах
-var dice = Math.floor(Math.random() * 6) + 1;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
 //<div class="player-score" id="score-0">43</div>
 //window.document.querySelector( "#score-0").textContent = dice;
 
 //document.querySelector( "#score-1").textContent = dice;
+//програм эхлэхэд бэлтгэх
+document.getElementById("score-0").textContent = "0";
+document.getElementById( "score-1").textContent = "0";
+document.getElementById( "current-0").textContent = "0" ;
+document.getElementById( "current-1").textContent = "0" ;
+var diceDom = document.querySelector( " .dice ")
+diceDom.style.display = " none" ;
+document.querySelector(".btn-roll").addEventListener("click", function() {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector( " .dice ").style.display = " none" ;
-document.querySelector( "#score-1").textContent = 0;
-document.querySelector( "#score-0").textContent = 0;
+    diceDom.style.display = " block" ;
+    diceDom.src = "dice-" + diceNumber + ".png"
+    
 
-document.querySelector( "#current-0").textContent = 0 ;
-document.querySelector( "#current-1").textContent = 0 ;
+    
+});
 
-document.querySelector( " .dice ").style.display = " none" ;
-
-console.log( "Шоо :" + dice);
+function shooShid(){s
+    alert      
+}
